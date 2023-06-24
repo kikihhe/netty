118d26f0a7ce9fd069fc957b011292ebda310de5
2382546457 <2382546457@qq.com>
2023-06-24 17:25:07 +0800

    Buffer

---
+++ netty-01-io/src/NIO/TestBuffer.java
@@ -0,0 +1,40 @@
+package NIO;
+
+import java.nio.*;
+
+/**
+ * @ClassName TestBuffer
+ * @Description NIO的使用
+ * @Author 何
+ * @Date 2023-06-23 22:36
+ * @Version 1.0
+ */
+public class TestBuffer {
+    public static void main(String[] args) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+
+
+        byteBuffer.put((byte)5);
+        byteBuffer.put((byte)7);
+        byteBuffer.put((byte)2);
+        byteBuffer.put((byte)6);
+        byteBuffer.put((byte)4);
+
+
+        // 将limit = position
+        // 将position = 0
+        byteBuffer.clear();
+        byteBuffer.rewind();
+        byteBuffer.compact();
+
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.get());
+
+
+
+
+    }
+}
